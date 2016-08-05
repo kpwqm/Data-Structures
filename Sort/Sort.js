fa1ee81fb77571f462aef9aa2e53027638715829
Man Wu <kpwqm@163.com>
2016-08-06 01:20:51 +0800

    update sort

--- Sort/Sort.js
+++ Sort/Sort.js
@@ -15,6 +15,9 @@ var CArray = function (numElements){
 	}
 	this.bubbleSort = bubbleSort;
 	this.selectionSort = selectionSort;
+	this.insertionSort = insertionSort;
+	this.gaps = [5,3,1];
+	this.shellSort = shellSort;
 }
 var setData = function (){
 	for (var i = 0; i < this.numElements; i++){
@@ -79,10 +82,50 @@ var selectionSort = function (){
 		}
 	}
 }
-var numElements = 10;
-var mynums = new CArray(numElements);
-mynums.setData();
-console.log(mynums.toString());
-mynums.selectionSort();
+//demo
+/*myNums.selectionSort();
+console.log(myNums.toString())*/
+/**
+ * [insertionSort() - 插入排序]
+ * 插入排序对于少量元素比较有效。一定情况下效率比选择排序和冒泡排序更高
+ * 插入排序有两个循环，外循环将数组元素挨个移动，而内循环则对外循环中选中的元素及它后面的那个元素进行比较
+ */
+var insertionSort = function (){
+	var temp, inner;
+	for (var outer = 1, l = this.dataStore.length; outer < l; outer++){
+		temp = this.dataStore[outer];
+		inner = outer - 1;
+		while (inner > -1 && (this.dataStore[inner] > temp)){
+			this.dataStore[inner + 1] = this.dataStore[inner];
+			inner--;
+		}
+		this.dataStore[inner + 1] = temp;
+	}
+}
+//demo
+/*var numElements = 100;
+var myNums = new CArray(numElements);
+myNums.setData();
+console.log(myNums.toString())
+myNums.insertionSort();
+console.log(myNums.toString())*/
+/**
+ * [shellSort() - 希尔排序]
+ * 通过定义一个间隔序列来表示在排序过程中进行比较的元素之间有多远的间隔。
+ * 参考间隔: 701, 301, 132, 57, 23, 10, 4, 1
+ */
+var shellSort = function (){
+	for (var g = 0, len = this.gaps.length; g < len; g++){
+		for (var i = 0, l = this.dataStore.length; i < l; i++){
+			var temp = this.dataStore[i];
+			for (var j = i; j >= this.gaps[g] && this.dataStore[j - this.gaps[g]] > temp; j -= this.gaps[g]){
+				this.dataStore[j] = this.dataStore[j - this.gaps[g]];
+			}
+			this.dataStore[j] = temp;
+		}
+	}
+}
+//demo
+/*myNums.shellSort();
 console.log()
-console.log(mynums.toString())
+console.log(myNums.toString())*/
